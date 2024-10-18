4e8accd29b936790a4fab61686c721c9a472ee9f
Piers Chua <2201999F@student.tp.edu.sg>
2024-10-18 21:04:45 +0800

    added disposals entity

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.BASE_URL}/new-verification?token=${token}`;
