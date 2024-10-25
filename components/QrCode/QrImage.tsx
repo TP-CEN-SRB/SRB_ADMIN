af4f3a1c6fe7b3b22cd2590079309ad44b6a782a
PiersChua <2201999F@student.tp.edu.sg>
2024-10-25 10:30:01 +0800

    deployment issues

--- components/QrCode/QrImage.tsx
+++ components/QrCode/QrImage.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const QrCodeComponent = async ({ id }: { id: string }) => {
   const disposalData = await getUnscannedDisposal(id);
-  if (!disposalData) {
+  if (!disposalData || "error" in disposalData) {
     notFound();
   }
   const data = {
