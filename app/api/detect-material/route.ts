be1e042d8f47b465c9a11a60c1e3cd7b8b9a197a
PiersChua <2201999F@student.tp.edu.sg>
2024-10-27 12:46:51 +0800

    changed max duration for sse to 60 seconds

--- app/api/detect-material/route.ts
+++ app/api/detect-material/route.ts
@@ -1,7 +1,7 @@
 /*
  * SSE
  */
-export const maxDuration = 150; // This function can run for a maximum of 150 seconds
+export const maxDuration = 60; // This function can run for a maximum of 150 seconds
 import { NextRequest, NextResponse } from "next/server";
 // export const runtime = "edge";
 type Client = {
