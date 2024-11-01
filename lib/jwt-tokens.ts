4e069e725076f0dcf771820dc94ce9175f26d0c0
PiersChua <2201999F@student.tp.edu.sg>
2024-11-01 20:44:12 +0800

    update readme

--- lib/jwt-tokens.ts
+++ lib/jwt-tokens.ts
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken";
-export const generateQrToken = (data: { disposalId: string }) => {
+export const generateQrToken = (data: {
+  disposalId: string;
+  userId: string;
+  material: string;
+  weightInGrams: number;
+}) => {
   const token = jwt.sign(data, process.env.NEXT_JWT_SECRET_KEY!, {
     expiresIn: "1h",
   });
