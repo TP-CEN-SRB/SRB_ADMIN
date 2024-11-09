134ee3cea9cc47d0e4646c884382c0012b35c970
PiersChua <2201999F@student.tp.edu.sg>
2024-11-09 14:10:46 +0800

    refactored custom form messages to alerts

--- components/Form/CustomFormMessage.tsx
+++ components/Form/CustomFormMessage.tsx
@@ -1,22 +1,28 @@
 import { ReactNode } from "react";
 import { LuCheckCircle } from "react-icons/lu";
 import { FaExclamationTriangle } from "react-icons/fa";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 interface FormMessageProps {
   children: ReactNode;
   type: "Error" | "Success";
 }
 const CustomFormMessage = ({ children, type }: FormMessageProps) => {
   return (
-    <div
-      className={`${
+    <Alert
+      className={`border ${
         type === "Error"
-          ? "bg-destructive/15 text-destructive"
-          : "bg-emerald-500/15 text-emerald-500"
-      } rounded-md flex items-center gap-x-2 text-sm p-3`}
+          ? "bg-destructive/15 text-destructive border-destructive "
+          : "bg-emerald-500/15 text-emerald-500 border-emerald-500"
+      }`}
     >
-      {type === "Error" ? <FaExclamationTriangle /> : <LuCheckCircle />}
-      <p>{children}</p>
-    </div>
+      <AlertTitle className="font-bold">
+        <div className="flex items-center gap-x-2">
+          {type === "Error" ? <FaExclamationTriangle /> : <LuCheckCircle />}
+          {type}
+        </div>
+      </AlertTitle>
+      <AlertDescription>{children}</AlertDescription>
+    </Alert>
   );
 };
 
