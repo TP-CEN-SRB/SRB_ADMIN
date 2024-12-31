3c5a5102348f925da0e6416bc3d552b733ab76f5
PiersChua <2201999F@student.tp.edu.sg>
2024-12-31 18:52:07 +0800

    changed updating of bin capacity to use parallel transaction

--- app/api/bin-capacity/[id]/route.ts
+++ app/api/bin-capacity/[id]/route.ts
@@ -14,6 +14,7 @@ export const PUT = async (
         { status: 401 }
       );
     }
+
     const id = params.id;
     const binManager = await prisma.user.findUnique({ where: { id: id } });
     if (!binManager) {
@@ -22,13 +23,15 @@ export const PUT = async (
         { status: 404 }
       );
     }
+
     /**
      * Expects an array as follows
      * [
-          { "material": "MATERIAL", "binCapacity": VALUE },
-       ]
+     *   { "material": "MATERIAL", "binCapacity": VALUE },
+     * ]
      */
     const data = await req.json();
+
     // Array of warning emails to send if the capacity > 85
     const emailsToSend: {
       emails: string[];
@@ -37,68 +40,91 @@ export const PUT = async (
       location: string;
     }[] = [];
 
-    await prisma.$transaction(async (transaction) => {
-      for (const { material, binCapacity } of data) {
-        const binMaterial = await transaction.binMaterial.findUnique({
-          where: { name: material.toUpperCase() },
-        });
-
-        if (!binMaterial) {
-          throw new Error(`Bin material: ${material} is not found!`);
-        }
-        // Find the specific bin associated with the userId and binMaterialId
-        const bin = await transaction.bin.findUnique({
-          where: {
-            userId_binMaterialId_status: {
-              userId: id,
-              binMaterialId: binMaterial.id,
-              status: "FUNCTIONAL",
-            },
-          },
-          include: { binMaterial: true, user: true },
-        });
-        if (!bin) {
-          throw new Error(
-            `No functional bin found for material: ${material}}!`
-          );
-        }
-        // Update the bin's capacity
-        await transaction.bin.update({
-          where: { id: bin.id },
-          data: { currentCapacity: parseFloat(binCapacity.toFixed(2)) },
-        });
-        // Populate the email array if bin is almost full and warning email has not been sent
-        if (binCapacity > 85 && !bin.emailSent) {
-          const subscriptions = await transaction.subscription.findMany({
-            where: { userId: bin.userId },
-          });
-          if (subscriptions.length > 0) {
-            emailsToSend.push({
-              emails: subscriptions.map((subscription) => subscription.email),
+    await prisma.$transaction(
+      async (transaction) => {
+        await Promise.all(
+          data.map(
+            async ({
+              material,
               binCapacity,
-              material: bin.binMaterial.name,
-              location: bin.user.location as string,
-            });
-            await transaction.bin.update({
-              where: { id: bin.id },
-              data: { emailSent: true },
-            });
-          }
-        }
-        // Clear the email flag is bin is cleared and warning email was sent previously
-        else if (binCapacity < 85 && bin.emailSent) {
-          await transaction.bin.update({
-            where: { id: bin.id },
-            data: { emailSent: false },
-          });
-        }
-      }
-    });
+            }: {
+              material: string;
+              binCapacity: number;
+            }) => {
+              const binMaterial = await transaction.binMaterial.findUnique({
+                where: { name: material.toUpperCase() },
+              });
+
+              if (!binMaterial) {
+                throw new Error(`Bin material: ${material} is not found!`);
+              }
+
+              const bin = await transaction.bin.findUnique({
+                where: {
+                  userId_binMaterialId_status: {
+                    userId: id,
+                    binMaterialId: binMaterial.id,
+                    status: "FUNCTIONAL",
+                  },
+                },
+                include: { binMaterial: true, user: true },
+              });
+
+              if (!bin) {
+                throw new Error(
+                  `No functional bin found for material: ${material}!`
+                );
+              }
+
+              // Update the bin's capacity
+              await transaction.bin.update({
+                where: { id: bin.id },
+                data: { currentCapacity: parseFloat(binCapacity.toFixed(2)) },
+              });
+
+              // Populate the email array if bin is almost full and warning email has not been sent
+              if (binCapacity > 85 && !bin.emailSent) {
+                const subscriptions = await transaction.subscription.findMany({
+                  where: { userId: bin.userId },
+                });
+
+                if (subscriptions.length > 0) {
+                  emailsToSend.push({
+                    emails: subscriptions.map(
+                      (subscription) => subscription.email
+                    ),
+                    binCapacity,
+                    material: bin.binMaterial.name,
+                    location: bin.user.location as string,
+                  });
+
+                  await transaction.bin.update({
+                    where: { id: bin.id },
+                    data: { emailSent: true },
+                  });
+                }
+              }
+
+              // Clear the email flag if bin is cleared and warning email was sent previously
+              if (binCapacity < 85 && bin.emailSent) {
+                await transaction.bin.update({
+                  where: { id: bin.id },
+                  data: { emailSent: false },
+                });
+              }
+            }
+          )
+        );
+      },
+    );
+
+    // Send emails outside the transaction
     await Promise.all(
       emailsToSend.map(({ emails, binCapacity, material, location }) =>
         sendBinWarningEmail(emails, binCapacity, material, location)
       )
     );
+
     return NextResponse.json(
       { message: "Bin capacity updated successfully!" },
       { status: 200 }
