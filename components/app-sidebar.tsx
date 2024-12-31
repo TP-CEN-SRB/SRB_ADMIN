6b7b976a0191488114fdcef45ef59b35cd3d58c9
PiersChua <2201999F@student.tp.edu.sg>
2024-12-31 21:56:29 +0800

    enabled modal

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -198,7 +198,7 @@ export function AppSidebar({ email }: { email: string | null | undefined }) {
         <SidebarFooter>
           <SidebarMenu>
             <SidebarMenuItem>
-              <DropdownMenu modal={false}>
+              <DropdownMenu modal={true}>
                 <DropdownMenuTrigger asChild>
                   <SidebarMenuButton>
                     <CgProfile /> {email}
@@ -207,6 +207,7 @@ export function AppSidebar({ email }: { email: string | null | undefined }) {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" side="right" sideOffset={10}>
                   <DropdownMenuLabel>Admin Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
                   {dropdownItems.map((item, index) => (
                     <DropdownMenuItem
                       key={index}
@@ -219,6 +220,7 @@ export function AppSidebar({ email }: { email: string | null | undefined }) {
                       </Link>
                     </DropdownMenuItem>
                   ))}
+                  <DropdownMenuSeparator />
                   <DropdownMenuItem
                     className="flex items-center gap-x-2 cursor-pointer"
                     onClick={() => setSignOutDialogOpen(true)}
