2b89c912087f3cd1b7377ad28b18c239f453aa36
PiersChua <2201999F@student.tp.edu.sg>
2025-01-31 17:21:33 +0800

    added api to get disposals for bin manager

--- app/api/disposal/route.ts
+++ app/api/disposal/route.ts
@@ -3,6 +3,61 @@ import { DisposalSchema } from "@/schemas";
 import jwt from "jsonwebtoken";
 import prisma from "@/lib/db";
 
+export const GET = async (req: NextRequest) => {
+  try {
+    const token = req.headers.get("Authorization")?.split(" ")[1];
+    if (!token) {
+      return NextResponse.json(
+        { message: "Missing authorization header!" },
+        { status: 401 }
+      );
+    }
+    const decodedToken = jwt.verify(token, process.env.NEXT_JWT_SECRET_KEY!);
+    if (typeof decodedToken === "string") {
+      return NextResponse.json(
+        { message: "Unauthorized access!" },
+        { status: 401 }
+      );
+    }
+    const bins = await prisma.bin.findMany({
+      where: { userId: decodedToken.userId },
+      select: {
+        _count: { select: { disposals: true } },
+        binMaterial: { select: { name: true } },
+      },
+    });
+    if (bins.length === 0) {
+      return NextResponse.json({ message: "No bins found!" });
+    }
+    const disposals = bins.map((bin) => ({
+      material: bin.binMaterial.name,
+      count: bin._count.disposals,
+    }));
+    if (disposals.length === 0) {
+      return NextResponse.json({ message: "No disposals found!" });
+    }
+    return NextResponse.json({ disposals }, { status: 200 });
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      return NextResponse.json(
+        { message: "Token has expired!" },
+        { status: 401 }
+      );
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      return NextResponse.json(
+        { message: "Token is invalid!" },
+        { status: 401 }
+      );
+    } else if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { message: "An unknown error occurred" },
+      { status: 500 }
+    );
+  }
+};
+
 // sent by locally hosted bin
 export const POST = async (req: NextRequest) => {
   try {
@@ -13,6 +68,7 @@ export const POST = async (req: NextRequest) => {
         { status: 401 }
       );
     }
+
     const decodedToken = jwt.verify(token, process.env.NEXT_JWT_SECRET_KEY!);
     if (typeof decodedToken === "string") {
       return NextResponse.json(
