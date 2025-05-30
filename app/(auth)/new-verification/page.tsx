5e8329a7286a3eadc2c39a14d67e7d818ad6b004
TP-CEN-SRB <tpcensrb@gmail.com>
2025-05-30 16:57:55 +0800

    Add dynamic route for /new-verification/token/[token]

--- app/(auth)/new-verification/page.tsx
+++ app/(auth)/new-verification/page.tsx
@@ -2,13 +2,13 @@ import NewVerificationForm from "@/components/Form/AuthForms/NewVerificationForm
 import React from "react";
 
 const NewVerificationPage = ({
-  searchParams,
+  params,
 }: {
-  searchParams: { token: string };
+  params: { token: string };
 }) => {
   return (
     <div className="min-h-screen flex items-center justify-center container mx-auto max-w-screen-xs p-4">
-      <NewVerificationForm token={searchParams.token} />
+      <NewVerificationForm token={params.token} />
     </div>
   );
 };
