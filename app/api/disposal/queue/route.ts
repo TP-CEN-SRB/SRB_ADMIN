f11ef89513a319346691bc2237dfd80833d8bfc8
TP-CEN-SRB <tpcensrb@gmail.com>
2025-08-28 16:57:26 +0800

    Queue API

--- app/api/disposal/queue/route.ts
+++ app/api/disposal/queue/route.ts
@@ -1,18 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/db";
-import jwt from "jsonwebtoken";
 
 export const POST = async (req: NextRequest) => {
   try {
-    const token = req.headers.get("Authorization")?.split(" ")[1];
-    if (!token) return NextResponse.json({ message: "Missing authorization header!" }, { status: 401 });
-    const decoded = jwt.verify(token, process.env.NEXT_JWT_SECRET_KEY!);
-    if (typeof decoded === "string") return NextResponse.json({ message: "Unauthorized access!" }, { status: 401 });
+    const apiKey = req.headers.get("x-api-key");
+    if (apiKey !== process.env.API_KEY) {
+      return NextResponse.json({ message: "Permission denied" }, { status: 401 });
+    }
 
-       const q = await prisma.disposalQueue.create({ data: {} });
-    return NextResponse.json({ queueId: q.id }, { status: 200 });
+    // Create a new disposal queue
+    const queue = await prisma.disposalQueue.create({ data: {} });
+
+    return NextResponse.json({ queue }, { status: 200 });
   } catch (error) {
-    const message = error instanceof Error ? error.message : "An unknown error occurred";
-    return NextResponse.json({ message }, { status: 500 });
+    return NextResponse.json(
+      { message: error instanceof Error ? error.message : "Unknown error" },
+      { status: 500 }
+    );
   }
 };
