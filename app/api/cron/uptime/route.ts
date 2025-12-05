f64d31210aa91d57dfca08e069f2b900692d46a0
TP-CEN-SRB <tpcensrb@gmail.com>
2025-12-05 10:18:30 +0800

    Uptime graph using redis instead fix 2

--- app/api/cron/uptime/route.ts
+++ app/api/cron/uptime/route.ts
@@ -5,6 +5,8 @@ import { redis } from "@/lib/redis"; // ← NEW
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 
 
 export async function GET() {
--- app/api/uptime/route.ts
+++ app/api/uptime/route.ts
@@ -4,6 +4,7 @@ import prisma from "@/lib/db"; // only used to get bin list
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 // ---------------------
 // FORMATTERS
